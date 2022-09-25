f8abc29c07d8ae30c1fb9cbf9895cec940c70c52
EduardSergienko <katleraster@gmail.com>
2022-09-25 16:00:31 +0300

    Завершив виконання ДЗ-3

---
+++ models/contact.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+
+const contactShema = new Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "Set name for contact"],
+		},
+		email: {
+			type: String,
+		},
+		phone: {
+			type: String,
+			unique: true,
+		},
+		favorite: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{ versionKey: false }
+);
+
+const handleErrors = (error, _, next) => {
+	const { name, code } = error;
+	if (name === "MongoServerError" && code === 11000) {
+		error.status = 409;
+	} else {
+		error.status = 400;
+	}
+
+	next();
+};
+contactShema.post("save", handleErrors);
+const Contact = model("contact", contactShema);
+module.exports = Contact;
